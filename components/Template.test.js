947de83a21ff4a3e20bc93b52ab89122129f6003
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-12-04 14:49:41 +0900

    Enable absolute path imports for Jest with jest.config.js
    Stop using package.json to configure Jest because a Node module `path` needs to be imported to configure absolute path imports

--- components/Template.test.js
+++ components/Template.test.js
@@ -3,20 +3,26 @@ import {render} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import {axe} from 'jest-axe';
 
-import Template from './Template';
+import Template from 'components/Template';
 
 const mockProps = {};
 
 // DELETE THIS LINE
-describe.skip('delete this line', () => {
-  test('renders UI correctly', () => {
-    const {container} = render(<Template {...mockProps} />);
-    expect(container).toMatchInlineSnapshot();
-  });
+// describe.skip('delete this line', () => {
+test('renders UI correctly', () => {
+  const {container} = render(<Template {...mockProps} />);
+  expect(container).toMatchInlineSnapshot(`
+    <div>
+      <div>
+        Welcome to Next.js!
+      </div>
+    </div>
+  `);
+});
 
-  test('is accessible', async () => {
-    const {container} = render(<Template {...mockProps} />);
-    const results = await axe(container);
-    expect(results).toHaveNoViolations();
-  });
-}); // DELETE THIS LINE
+test('is accessible', async () => {
+  const {container} = render(<Template {...mockProps} />);
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+});
+// }); // DELETE THIS LINE
