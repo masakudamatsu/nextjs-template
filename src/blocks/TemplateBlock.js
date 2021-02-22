ed137a245e47b62eb4e688f66273895cf94165b0
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-02-22 20:58:22 +0900

    feat(templateelement.js templateblock.js): add proptypes to block and element component templates
    allow block and element components to handle PropTypes

--- src/blocks/TemplateBlock.js
+++ src/blocks/TemplateBlock.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const TemplateBlock = styled.div``;
+
+TemplateBlock.propTypes = {};
 
 export default TemplateBlock;
--- src/elements/TemplateElement.js
+++ src/elements/TemplateElement.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const TemplateElement = styled.div``;
 
+TemplateElement.propTypes = {};
 export default TemplateElement;
